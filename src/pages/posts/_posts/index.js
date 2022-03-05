d1804d9e3bbd7b22199e289a91dbe5edcdc7e0de
mjs <jiasheng.ma@yahoo.com>
2022-03-04 21:31:15 -0500

    (WIP)

--- src/pages/posts/_posts/index.js
+++ src/pages/posts/_posts/index.js
@@ -1,5 +1,6 @@
-export { default as post11182021 } from "./post11182021";
-export { default as post11202021 } from "./post11202021";
-export { default as post11272021 } from "./post11272021";
-export { default as post12132021 } from "./post12132021";
-export { default as post12232021 } from "./post12232021";
+export { default as post11182021 } from "./beginning-of-posts";
+export { default as post11202021 } from "./on-art";
+export { default as post11272021 } from "./overview-of-app-development";
+export { default as post12132021 } from "./running-reading-list";
+export { default as post12232021 } from "./connections-within-docker-containers";
+export { default as post12232021 } from "./code-coverage-vs-test-coverage";
