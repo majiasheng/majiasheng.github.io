4a289811aff5abe7d1e151a642e21591c32467e7
mjs <jiasheng.ma@yahoo.com>
2022-12-29 22:57:01 -0500

    add image tool (disabling for now)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,5 @@ export { Home } from './home';
 export { About } from './about';
 export { Exhibitions } from './exhibitions';
 export { Posts } from './posts';
+export { Tools } from './tools';
 // export { Work } from './work';
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import {
   About,
   Exhibitions,
   Posts,
-//   Work,
+  // Tools,
+  // Work,
 } from './pages';
 
 export default {
@@ -11,5 +12,6 @@ export default {
   about: { path: '/about', name: 'About', component: About },
   exhibitions: { path: '/exhibitions', name: 'Exhibitions', component: Exhibitions },
   posts: { path: '/posts', name: 'Posts', component: Posts },
+  // tools: { path: '/__tools__', name: 'Tools', component: Tools },
   // work: {path: '/work', name: 'Work', component: Work},
 };
